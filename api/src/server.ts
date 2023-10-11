7c3dda2ef4c8e41e526be00928c25fca5ecea7ce
johnbrow10 <iroman250@gmail.com>
2023-10-10 23:43:46 -0300

    feat(create transcription): adicionar rotas de criação de trascrição conectdada com a api do openai
    usa a api do openai para criar transcrição do video ja hospedado e depois cadastra no banco de dados

--- api/src/server.ts
+++ api/src/server.ts
@@ -2,12 +2,14 @@ import { fastify } from 'fastify';
 import { prisma } from './lib/prisma';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
 
-const app = fastify();
+const app = fastify()
 
 // todo register obriga que a rota seja async
-app.register(getAllPromptsRoute);
+app.register(getAllPromptsRoute)
 app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
